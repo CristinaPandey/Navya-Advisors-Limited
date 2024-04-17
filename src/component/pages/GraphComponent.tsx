6e75f55e4dfbed321112177a7fa5835bc3682062
Cristina Pandey <104251773+CristinaPandey@users.noreply.github.com>
2024-04-17 21:57:32 +0545

    ..

--- src/component/pages/GraphComponent.tsx
+++ src/component/pages/GraphComponent.tsx
@@ -4,8 +4,8 @@ import { useStockData } from '../api/useData.tsx';
 
 import { CategoryScale, Chart } from "chart.js/auto";
 
-
 Chart.register(CategoryScale);
+
 const GraphComponent: React.FC = () => {
     const { isLoading, isError, data } = useStockData();
     console.log("graph", data);
@@ -15,15 +15,54 @@ const GraphComponent: React.FC = () => {
 
     const weeklyTimeSeries = data.data['Weekly Adjusted Time Series'];
     const dates = Object.keys(weeklyTimeSeries).slice(0, 7);
-    const weeklyData = dates.map(date => weeklyTimeSeries[date]);
-    const openPrices = weeklyData.map(item => parseFloat(item['1. open']));
-
     const chartData = {
         labels: dates.reverse(),
         datasets: [
             {
                 label: 'Open Price',
-                data: openPrices.reverse(),
+                data: dates.map(date => parseFloat(weeklyTimeSeries[date]['1. open'])).reverse(),
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1,
+            },
+            {
+                label: 'High',
+                data: dates.map(date => parseFloat(weeklyTimeSeries[date]['2. high'])).reverse(),
+                fill: false,
+                borderColor: 'rgb(192, 75, 75)',
+                tension: 0.1,
+            },
+            {
+                label: 'Low',
+                data: dates.map(date => parseFloat(weeklyTimeSeries[date]['3. low'])).reverse(),
+                fill: false,
+                borderColor: 'rgb(75, 75, 192)',
+                tension: 0.1,
+            },
+            {
+                label: 'Close',
+                data: dates.map(date => parseFloat(weeklyTimeSeries[date]['4. close'])).reverse(),
+                fill: false,
+                borderColor: 'rgb(192, 192, 75)',
+                tension: 0.1,
+            },
+            {
+                label: 'Adjusted Close',
+                data: dates.map(date => parseFloat(weeklyTimeSeries[date]['5. adjusted close'])).reverse(),
+                fill: false,
+                borderColor: 'rgb(75, 192, 75)',
+                tension: 0.1,
+            },
+            {
+                label: 'Volume',
+                data: dates.map(date => parseInt(weeklyTimeSeries[date]['6. volume'])).reverse(),
+                fill: false,
+                borderColor: 'rgb(192, 75, 192)',
+                tension: 0.1,
+            },
+            {
+                label: 'Dividend Amount',
+                data: dates.map(date => parseFloat(weeklyTimeSeries[date]['7. dividend amount'])).reverse(),
                 fill: false,
                 borderColor: 'rgb(75, 192, 192)',
                 tension: 0.1,
